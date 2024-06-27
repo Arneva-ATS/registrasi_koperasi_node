30e0fe34bdb310a02cb33c274d011df905f61840
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-06-27 21:50:25 +0700

    udpate

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const apps = express();
 const bodyParser = require('body-parser')
 const port = process.env.PORT;
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const cookieParser = require("cookie-parser");
+// const md5 = require('md5');
 
 apps.use(bodyParser.json())
 apps.use(
@@ -11,14 +14,30 @@ apps.use(
         extended: true,
     })
 )
+apps.use(cookieParser());
 
 apps.use(express.static('public'));
-
 apps.get('/', (req, res) => {
+    // console.log(md5('12345'));
     res.sendFile(path.resolve('./views/index.html'));
 })
 
+apps.post('/dologin', (req, res) => {
+    const { email, password } = req.body;
+    if (email == 'admin@arneva.co.id' && password == '12345') {
+        const isLogin = true;
+        res.cookie('token', uuidv4());
+        res.cookie('islogin', isLogin);
+        res.cookie('roles', "administrator");
+        res.redirect('/dashboard');
+    } else {
+        res.redirect('/login');
+    }
+
+})
+
 apps.get('/dashboard', (req, res) => {
+    console.log(req.cookies);
     res.sendFile(path.resolve('./views/dashboard.html'));
 })
 
@@ -27,5 +46,11 @@ apps.get('/detail/:id', (req, res) => {
     // const idTx = req.params.id;
 })
 
+apps.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.clearCookie('roles');
+    res.clearCookie('islogin');
+    res.redirect('/');
+})
 
 apps.listen(port);
